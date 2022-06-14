13588889e44c3a361f1fff228a583eb8a388ecb6
GIACOMO REBECCHI <grebecchi98@gmail.com>
2022-06-14 11:39:10 +0200

    Structured make_dataset module

---
+++ src/data/make_dataset.py
@@ -0,0 +1,48 @@
+import shutil
+
+from .config import COORDINATES_TABLE, FULL_TABLE, IMAGES_TABLE, ITEMS_TABLE, Table
+from .join_tables import join
+from .storage import exists, get_S3_fs
+
+
+def download(table: Table) -> None:
+    S3 = get_S3_fs()
+    S3.download(table.remote_path, table.local_path, recursive=True)
+
+
+def make_table(table: Table, remote: bool) -> None:
+    opath = table.remote_path if remote else table.local_path
+    table.write_func(opath=opath, remote=table.remote, **table.kwargs)
+
+
+def main(overwrite: bool = False, remote: bool = True) -> None:
+    if exists(FULL_TABLE.local_path, local=True):
+        if overwrite:
+            try:
+                shutil.rmtree(FULL_TABLE.local_path)
+            except OSError as e:
+                print("Error: %s - %s." % (e.filename, e.strerror))
+        else:
+            raise Exception("Data has already been downloaded.")
+    if exists(FULL_TABLE.remote_path, local=False):
+        download(FULL_TABLE)
+    else:
+        if exists(COORDINATES_TABLE.remote_path, local=False):
+            pass
+        else:
+            COORDINATES_TABLE.remote = remote
+            make_table(COORDINATES_TABLE, remote)
+
+        if exists(IMAGES_TABLE.remote_path, local=False):
+            pass
+        else:
+            IMAGES_TABLE.remote = remote
+            make_table(IMAGES_TABLE, remote)
+
+        if exists(ITEMS_TABLE, local=False):
+            pass
+        else:
+            ITEMS_TABLE.remote = remote
+            make_table(ITEMS_TABLE, remote)
+
+        join([ITEMS_TABLE, COORDINATES_TABLE, IMAGES_TABLE], remote)
