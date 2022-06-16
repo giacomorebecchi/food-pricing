ef6e9d7669ab152f42ade53d024fd78843367d42
GIACOMO REBECCHI <grebecchi98@gmail.com>
2022-06-16 18:19:49 +0200

    Utils to clean tables in local and remote

---
+++ src/data/clean_dataset.py
@@ -0,0 +1,20 @@
+import shutil
+from pathlib import PurePosixPath
+
+from src.data.config import DATASET, FULL_TABLE
+from src.data.config_interim import COORDINATES_TABLE, IMAGES_TABLE, ITEMS_TABLE
+from src.data.storage import exists, get_S3_fs
+from src.data.table_model import Table
+
+
+def clean_table(table: Table):
+    if exists(table.remote_path, local=False):
+        S3 = get_S3_fs()
+        S3.rm(str(table.remote_path), recursive=True, maxdepth=None)
+    if exists(table.local_path, local=True):
+        shutil.rmtree(table.local_path)
+
+
+if __name__ == "__main__":
+    for table in [COORDINATES_TABLE, IMAGES_TABLE, ITEMS_TABLE, FULL_TABLE, DATASET]:
+        clean_table(table)
