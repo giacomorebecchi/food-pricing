a469f3f002a1346795a4e796de50a72a5c31117c
GIACOMO REBECCHI <grebecchi98@gmail.com>
2022-06-20 15:33:45 +0200

    Completed dask dummy version of Dataset #7

--- src/models/utils/data.py
+++ src/models/utils/data.py
@@ -1,7 +1,11 @@
+from io import BytesIO
 from typing import Dict
 
-import dask.dataframe as dd
+import torch
+import yaml
 from PIL import Image
+from src.data.config import DATASET
+from src.data.storage import CONFIG_PATH, dd_read_parquet, get_S3_fs
 from torch import is_tensor
 from torch.utils.data import Dataset
 
@@ -12,31 +16,45 @@ class FoodPricingDataset(Dataset):
         img_transform,
         txt_transform,
     ) -> None:
-        self.remote_data = False  # TODO: Understand if dataset is saved or from S3
-        self.remote_img = False  # TODO: Understand if images are saved or from S3
-        self.data = dd.DataFrame()  # TODO: Load dataset
-        self.img_folder = None  # TODO: Define image folder
+        self.config: Dict = yaml.safe_load(open(CONFIG_PATH))
+        self.dataset_remote = self.config.get("dataset_remote")
+        self.img_remote = self.config.get("img_remote")
+        dataset_path = (
+            DATASET.remote_path if self.dataset_remote else DATASET.local_path
+        )
+        self.data = dd_read_parquet(dataset_path, self.dataset_remote)
+        self.index = self.data.index.compute()
         self.img_transform = img_transform
         self.txt_transform = txt_transform
 
-    def __len__(self):
-        return len(self.data)
+    def __len__(self) -> int:
+        return len(self.index)
 
     def __getitem__(
         self,
-        idx,  # TODO: Understand idx type
-    ) -> Dict[str]:
+        idx,
+    ) -> Dict:
         if is_tensor(idx):
             idx = idx.tolist()
-        item_id = self.data.loc[idx, "id"]  # TODO: more efficient with index
-        img = Image.open(self.data.loc[idx, "imgPath"]).convert("RGB")
-        txt = self.data.loc[idx, "txt"]
-        coords = (self.data.loc[idx, "lat"], self.data.loc[idx, "lon"])
-        label = self.data.loc[idx, "price_fractional"]
+        idx = self.index[idx]
+        ddf = self.data.loc[idx, :].compute()
+        img = self.getimage(ddf.loc[idx, "imgPath"])
+        txt = ddf.loc[idx, "txt"]
+        lat = ddf.loc[idx, "lat"]
+        lon = ddf.loc[idx, "lon"]
+        label = ddf.loc[idx, "price_fractional"]
         return {
-            "id": item_id,  # TODO: Tensor
             "img": self.img_transform(img),
             "txt": self.txt_transform(txt),
-            "coords": coords,  # TODO: Tensor
-            "label": label,  # TODO: Tensor
+            "coords": torch.Tensor([lat, lon]),
+            "label": torch.IntTensor([label]),
         }
+
+    def getimage(self, path: str) -> Image:
+        if self.img_remote:
+            S3 = get_S3_fs()
+            with S3.open(path, mode="rb") as f:
+                img_bytes = f.read()
+            return Image.open(BytesIO(img_bytes)).convert("RGB")
+        else:
+            return Image.open(path).convert("RGB")
