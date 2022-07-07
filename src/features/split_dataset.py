6aec4b9124892d5560b6bdb069a102846b57a1b3
GIACOMO REBECCHI <grebecchi98@gmail.com>
2022-07-07 19:08:42 +0200

    Improved split_df #8

--- src/features/split_dataset.py
+++ src/features/split_dataset.py
@@ -1,11 +1,15 @@
 from typing import Tuple
 
+import dask.array as da
+import dask.dataframe as dd
 import numpy as np
 import pandas as pd
 
 
 def _check_train_dev_test_ratio(
-    train_ratio: float = 0.7, dev_ratio: float = None, test_ratio: float = None
+    train_ratio: float = 0.7,
+    dev_ratio: float = None,
+    test_ratio: float = None,
 ) -> Tuple[float]:
     def check_value(var_name: str, val: float) -> None:
         if not (isinstance(val, float) and 0 < val < 1):
@@ -27,20 +31,49 @@ def _check_train_dev_test_ratio(
     return train_ratio, dev_ratio, test_ratio
 
 
-def split_df(
-    df: pd.DataFrame, train_dev_test_ratio: Tuple[float] = (), seed: int = 42
-) -> pd.DataFrame:
-    train_ratio, dev_ratio, test_ratio = _check_train_dev_test_ratio(
-        *train_dev_test_ratio
+def dd_split_df(
+    ddf: dd.DataFrame,
+    train_dev_test_ratio: Tuple[float] = (),
+    seed: int = 42,
+) -> dd.DataFrame:
+    train_ratio, dev_ratio, test_ratio = _check_train_dev_test_ratio()
+    n = len(ddf)
+    train_n = int(n * train_ratio)
+    dev_n = int(n * dev_ratio)
+    test_n = n - train_n - dev_n
+    rng = np.random.default_rng(seed=42)
+    splitter = rng.permutation(
+        np.hstack(
+            [
+                np.zeros(train_n, dtype=np.int8),
+                np.ones(dev_n, dtype=np.int8),
+                np.ones(test_n, dtype=np.int8) + 1,
+            ]
+        )
     )
+    ddf = ddf.assign(split=da.array(splitter))
+    return ddf
+
+
+def pd_split_df(
+    df: pd.DataFrame,
+    train_dev_test_ratio: Tuple[float] = (),
+    seed: int = 42,
+) -> pd.DataFrame:
+    train_ratio, dev_ratio, test_ratio = _check_train_dev_test_ratio()
     n = len(df)
     train_n = int(n * train_ratio)
     dev_n = int(n * dev_ratio)
-    rng = np.random.default_rng(seed=seed)
-    train_dev = rng.choice(n, train_n + dev_n, replace=False)
-    train = train_dev[:train_n]
-    dev = train_dev[train_n:]
-    df["split"] = "test"
-    df.iloc[train, df.columns.get_loc("split")] = "train"
-    df.iloc[dev, df.columns.get_loc("split")] = "dev"
+    test_n = n - train_n - dev_n
+    rng = np.random.default_rng(seed=42)
+    splitter = rng.permutation(
+        np.hstack(
+            [
+                np.zeros(train_n, dtype=np.int8),
+                np.ones(dev_n, dtype=np.int8),
+                np.ones(test_n, dtype=np.int8) + 1,
+            ]
+        )
+    )
+    df = df.assign(split=splitter)
     return df
