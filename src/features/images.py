ab3f0e6faff7764e058f2fe0793f60114a7814d2
GIACOMO REBECCHI <grebecchi98@gmail.com>
2022-07-08 20:19:27 +0200

    Bug fixes in #10

--- src/features/images.py
+++ src/features/images.py
@@ -14,21 +14,25 @@ def save_img(
     s: dd.Series,
     S3: S3FileSystem,
     opath: PurePosixPath,
-    size: Tuple[int, int],
+    imgPath_column: str = "imgPath",
+    size: Tuple[int, int] = (450, 450),
 ) -> None:
     img_opath = PurePosixPath(opath).joinpath(s.name).with_suffix(".jpg")
-    with S3.open(s["imgPath"], mode="rb") as f_img:
+    with S3.open(s[imgPath_column], mode="rb") as f_img:
         img_byte_str = f_img.read()
     with Image.open(BytesIO(img_byte_str)) as im:
-        im.thumbnail(size)
-        im.save(str(img_opath), "JPEG")
+        try:
+            im.thumbnail(size)
+            im.save(str(img_opath), "JPEG")
+        except Exception as e:
+            print(f"Not able to download image {s[imgPath_column]}", e)
 
 
 def download_thumbnails(
     opath: PurePosixPath,
     remote: bool = False,
     raw_table: Table = Table(),
-    imgPath_column: Union[List[str], None] = "imgPath",
+    imgPath_column: str = "imgPath",
     size: Tuple[int, int] = (450, 450),
 ) -> None:
     path = raw_table.remote_path if raw_table.remote else raw_table.local_path
@@ -39,5 +43,7 @@ def download_thumbnails(
     )
     S3 = get_S3_fs()
     _ = ddf.apply(
-        lambda row: save_img(row, S3, opath, size), axis=1, meta=(None, "object")
+        func=lambda row: save_img(row, S3, opath, imgPath_column, size),
+        axis=1,
+        meta=(None, "object"),
     ).compute()
