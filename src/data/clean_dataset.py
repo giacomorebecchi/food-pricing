90ca02b5d5de5ec4fb5aa2722480dfc6172c303c
GIACOMO REBECCHI <grebecchi98@gmail.com>
2022-07-08 12:51:27 +0200

    Generalized clean_dataset module to DataObject #11

--- src/data/clean_dataset.py
+++ src/data/clean_dataset.py
@@ -1,20 +1,20 @@
 import shutil
-from pathlib import PurePosixPath
 
 from src.data.config import DATASET, FULL_TABLE
 from src.data.config_interim import COORDINATES_TABLE, IMAGES_TABLE, ITEMS_TABLE
 from src.data.storage import exists, get_S3_fs
-from src.data.table_model import Table
+from src.data.table_model import DataObject
 
 
-def clean_table(table: Table):
-    if exists(table.remote_path, local=False):
+def clean_dataobj(dataobj: DataObject):
+    if exists(dataobj.remote_path, local=False):
         S3 = get_S3_fs()
-        S3.rm(str(table.remote_path), recursive=True, maxdepth=None)
-    if exists(table.local_path, local=True):
-        shutil.rmtree(table.local_path)
+        S3.rm(str(dataobj.remote_path), recursive=True, maxdepth=None)
+    if exists(dataobj.local_path, local=True):
+        shutil.rmtree(dataobj.local_path)
 
 
 if __name__ == "__main__":
     for table in [COORDINATES_TABLE, IMAGES_TABLE, ITEMS_TABLE, FULL_TABLE, DATASET]:
-        clean_table(table)
+        clean_dataobj(table)
+    # TODO: add txt file here
