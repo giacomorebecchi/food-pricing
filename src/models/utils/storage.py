dbccd6a5cb0092ea9f88c05696cbeeb95f5e5c41
GIACOMO REBECCHI <grebecchi98@gmail.com>
2022-07-19 16:23:59 +0200

    New function to get best checkpoint path

--- src/models/utils/storage.py
+++ src/models/utils/storage.py
@@ -1,3 +1,4 @@
+import glob
 import os
 from pathlib import PurePosixPath
 from typing import List
@@ -21,3 +22,28 @@ def get_local_models_path(
         os.makedirs(models_path, exist_ok=False)
     path = models_path.joinpath(file_name).with_suffix(file_format)
     return path
+
+
+def get_best_checkpoint_path(
+    model_class: pl.LightningModule = None,
+    metric: str = "val_loss",
+    asc: bool = True,
+) -> str:
+    model_name = model_class.__name__ if model_class is not None else ""
+    current_path = PurePosixPath(__file__).parent
+    parent_path = current_path.parent.parent.parent.joinpath("models", model_name, "*")
+    checkpoints = [
+        path
+        for el in glob.glob(str(parent_path))
+        if os.path.isfile(path := PurePosixPath(el)) and path.suffix == ".ckpt"
+    ]
+    path_score = {
+        str(path): float(path.stem.split("=")[-1])
+        for path in checkpoints
+        if metric in path.stem
+    }
+    if asc:
+        best_checkpoint_path = min(path_score, key=path_score.get)
+    else:
+        best_checkpoint_path = max(path_score, key=path_score.get)
+    return best_checkpoint_path
