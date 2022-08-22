d63c89f9cb4d87fb244db03b61e766ca388f5235
giacomorebecchi <grebecchi98@gmail.com>
2022-08-22 16:19:32 +0000

    Added notifier module #40

---
+++ src/models/utils/notifier.py
@@ -0,0 +1,16 @@
+import logging
+
+from telegram.bot import Bot
+
+from ...data.settings import get_notifier_settings
+
+
+def send_telegram_message(message: str) -> None:
+    try:
+        bot_settings = get_notifier_settings()
+        bot = Bot(token=bot_settings.BOT_TOKEN)
+        bot.send_message(message)
+    except Exception as e:
+        logging.error(
+            f"Failed to send message: {message} with Telegram Bot.\nException: {e}"
+        )
