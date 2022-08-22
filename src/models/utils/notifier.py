fa94cd724b05f19dd0a43b8e6601be1b6c6140a6
giacomorebecchi <grebecchi98@gmail.com>
2022-08-22 16:39:37 +0000

    Fixed bug in notifier module #40

--- src/models/utils/notifier.py
+++ src/models/utils/notifier.py
@@ -9,7 +9,7 @@ def send_telegram_message(message: str) -> None:
     try:
         bot_settings = get_notifier_settings()
         bot = Bot(token=bot_settings.BOT_TOKEN)
-        bot.send_message(message)
+        bot.send_message(chat_id=bot_settings.CHAT_ID, text=message)
     except Exception as e:
         logging.error(
             f"Failed to send message: {message} with Telegram Bot.\nException: {e}"
