e1f626e44bf259303f14bed7149f230f3f3719da
giacomorebecchi <grebecchi98@gmail.com>
2022-08-23 21:45:20 +0000

    Fixed wrong change in callback XGB

--- src/models/utils/callbacks.py
+++ src/models/utils/callbacks.py
@@ -40,9 +40,7 @@ class TelegramBotCallback(Callback):
         self.n_epoch += 1
         self.epoch_start = datetime.now()
 
-    def on_validation_epoch_end(
-        self, trainer: Trainer, pl_module: LightningModule
-    ) -> None:
+    def on_train_epoch_end(self, trainer: Trainer, pl_module: LightningModule) -> None:
         if self.n_epoch == 0:  # This means we are in a sanity check phase
             return None
         self.val_losses.append(val_loss := pl_module.avg_val_loss)
@@ -51,8 +49,12 @@ class TelegramBotCallback(Callback):
             f"Validation finished for epoch {self.n_epoch} out of {self.max_epochs}",
             "Epoch duration: %s" % str(epoch_duration),
             "Obtained validation loss: %.2f" % val_loss,
-            "Obtained training loss: %.2f" % pl_module.avg_train_loss,
         ]
+        try:
+            train_loss = pl_module.avg_train_loss
+            contents.append("Obtained training loss: %.2f" % train_loss)
+        except Exception:
+            pass
         if self.n_epoch > 1:
             if val_loss < self.best_result:
                 improvement = self.best_result - val_loss
