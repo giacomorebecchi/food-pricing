76c8e48d741ee738b792d8c519998991056ac01c
giacomorebecchi <grebecchi98@gmail.com>
2022-08-23 16:10:33 +0000

    Utils param functions for PreTrainedResNet152 #43

--- src/models/vision/pretrained_resnet.py
+++ src/models/vision/pretrained_resnet.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Optional
+from typing import Generator, Optional
 
 from torch import Tensor, nn
 from torchvision.models import resnet152
@@ -14,8 +14,9 @@ class PreTrainedResNet152(nn.Module):
         super(PreTrainedResNet152, self).__init__()
         self.resnet = resnet152(weights=weights)
         in_features = self.resnet.fc.in_features
+        self.feature_dim = feature_dim
         self.freeze_encoder()
-        if feature_dim is not None:
+        if self.feature_dim is not None:
             self.resnet.fc = nn.Linear(
                 in_features=in_features, out_features=feature_dim
             )
@@ -40,6 +41,22 @@ class PreTrainedResNet152(nn.Module):
             for param in self.resnet.parameters():
                 param.requires_grad = True
             self.frozen = False
+
+    def get_general_params(self) -> Generator:
+        if self.feature_dim is None:
+            yield from ()
+        else:
+            for name, param in self.named_parameters():
+                if name in ["resnet.0.fc.weight", "resnet.0.fc.bias"]:
+                    yield param
+
+    def get_encoder_params(self) -> Generator:
+        if self.feature_dim is None:
+            yield from self.parameters()
+        else:
+            for name, param in self.named_parameters():
+                if name not in ["resnet.0.fc.weight", "resnet.0.fc.bias"]:
+                    yield param
 
     def forward(self, img: Tensor) -> Tensor:
         return self.resnet(img)
