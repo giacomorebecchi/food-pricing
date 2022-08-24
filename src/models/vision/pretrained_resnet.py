831514ea1fe48b29192ee804c0ea041a45830fa0
giacomorebecchi <grebecchi98@gmail.com>
2022-08-24 09:56:41 +0000

    Fixed PreTrainedResNet unfreezing #43

--- src/models/vision/pretrained_resnet.py
+++ src/models/vision/pretrained_resnet.py
@@ -12,35 +12,35 @@ class PreTrainedResNet152(nn.Module):
         feature_dim: Optional[int] = None,
     ):
         super(PreTrainedResNet152, self).__init__()
-        self.resnet = resnet152(weights=weights)
-        in_features = self.resnet.fc.in_features
+        resnet = resnet152(weights=weights)
+        in_features = resnet.fc.in_features
         self.feature_dim = feature_dim
-        self.freeze_encoder()
         if self.feature_dim is not None:
-            self.resnet.fc = nn.Linear(
-                in_features=in_features, out_features=feature_dim
-            )
+            resnet.fc = nn.Linear(in_features=in_features, out_features=feature_dim)
             logging.info(
                 "Substituting the last ResNet152 layer with a trainable "
                 "Fully Connected Layer to pass from "
                 f"{in_features} to {feature_dim} features."
             )
         self.resnet = nn.Sequential(
-            self.resnet,
+            resnet,
             nn.ReLU(),
         )
+        self.freeze_encoder()
 
     def freeze_encoder(self) -> None:
-        for param in self.resnet.parameters():
-            param.requires_grad = False
+        for name, param in self.named_parameters():
+            if name not in ["resnet.0.fc.weight", "resnet.0.fc.bias"]:
+                param.requires_grad = False
         self.frozen = True
 
     def unfreeze_encoder(self) -> None:
         if self.frozen:
-            logging.info(f"Encoder model {self.__class__.__name__} started fine-tuning")
-            for param in self.resnet.parameters():
-                param.requires_grad = True
+            for name, param in self.named_parameters():
+                if name not in ["resnet.0.fc.weight", "resnet.0.fc.bias"]:
+                    param.requires_grad = True
             self.frozen = False
+            logging.info(f"Encoder model {self.__class__.__name__} started fine-tuning")
 
     def get_general_params(self) -> Generator:
         if self.feature_dim is None:
